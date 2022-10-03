da67d3b4e3a6200556c963a5d3f978d04fbd78db
rorosario <rorosario@me.com>
2022-10-03 18:25:45 -0400

    adding_gateway

--- main.tf
+++ main.tf
@@ -4,6 +4,25 @@ resource "aws_vpc" "mtc_vpc" {
     enable_dns_support = true
 
     tags = {
-        name = "dev"
+        name = "dev_vpc"
     }
+}
+
+resource "aws_subnet" "mtc_public_subnet" {
+    vpc_id = aws_vpc.mtc_vpc.id
+    cidr_block = "10.123.1.0/24"
+    map_public_ip_on_launch = true
+    availability_zone = "us-east-1a"
+
+    tags = {
+        name = "dev-public"
+    }
+}
+
+resource "aws_internet_gateway" "mtc_internet_gateway" {
+  vpc_id = aws_vpc.mtc_vpc.id
+
+  tags = {
+    Name = "dev_igw"
+  }
 }
