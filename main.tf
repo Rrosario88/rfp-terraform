ad2916a4571d30d2bb6de434cb3fce07680e8122
rorosario <rorosario@me.com>
2022-11-18 16:04:08 -0500

    adding EC2 instance

--- main.tf
+++ main.tf
@@ -80,3 +80,20 @@ resource "aws_key_pair" "mtc_auth" {
     name = "dev_kp"
   }
 }
+
+resource "aws_instance" "dev_node" {
+  instance_type = "t2.micro"
+ami = data.aws_ami.server_ami.id
+
+tags = {
+  name = "dev-node"
+}
+
+key_name = aws_key_pair.mtc_auth.id
+vpc_security_group_ids = [aws_security_group.mtc_sg.id]
+subnet_id = aws_subnet.mtc_public_subnet.id
+
+root_block_device {
+  volume_size = 10
+}
+}
