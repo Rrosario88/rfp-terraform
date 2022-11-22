b2bd07e53bf84971343085dac11a8a6bf6dccd94
rorosario <rorosario@me.com>
2022-11-21 20:10:15 -0500

    formatting code

--- main.tf
+++ main.tf
@@ -84,11 +84,6 @@ resource "aws_key_pair" "mtc_auth" {
 resource "aws_instance" "dev_node" {
   instance_type = "t2.micro"
 ami = data.aws_ami.server_ami.id
-
-tags = {
-  name = "dev-node"
-}
-
 key_name = aws_key_pair.mtc_auth.id
 vpc_security_group_ids = [aws_security_group.mtc_sg.id]
 subnet_id = aws_subnet.mtc_public_subnet.id
@@ -96,4 +91,11 @@ subnet_id = aws_subnet.mtc_public_subnet.id
 root_block_device {
   volume_size = 10
 }
+
+tags = {
+  name = "dev-node"
+}
+
+
+
 }
