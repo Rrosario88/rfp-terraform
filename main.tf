aced31359eefe1783851c1699e28cd3a933655a1
rorosario <rorosario@me.com>
2022-11-22 22:40:04 -0500

    formmatting

--- main.tf
+++ main.tf
@@ -82,19 +82,19 @@ resource "aws_key_pair" "mtc_auth" {
 }
 
 resource "aws_instance" "dev_node" {
-  instance_type = "t2.micro"
-ami = data.aws_ami.server_ami.id
-key_name = aws_key_pair.mtc_auth.id
-vpc_security_group_ids = [aws_security_group.mtc_sg.id]
-subnet_id = aws_subnet.mtc_public_subnet.id
+  instance_type          = "t2.micro"
+  ami                    = data.aws_ami.server_ami.id
+  key_name               = aws_key_pair.mtc_auth.id
+  vpc_security_group_ids = [aws_security_group.mtc_sg.id]
+  subnet_id              = aws_subnet.mtc_public_subnet.id
 
-root_block_device {
-  volume_size = 10
-}
+  root_block_device {
+    volume_size = 10
+  }
 
-tags = {
-  name = "dev-node"
-}
+  tags = {
+    name = "dev-node"
+  }
 
 
 
